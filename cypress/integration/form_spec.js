8129cf9663f2955d6ce4d3ac0dc55bbfa94b4ff6
Alexis Newby <alexis.newby@gmail.com>
2022-02-16 11:18:52 -0700

    Add tests checking form values

---
+++ cypress/integration/form_spec.js
@@ -0,0 +1,25 @@
+describe('Form Functionality', () => {
+    it('Should be able to fill out the form and see those values reflected in the form inputs', () => {
+        cy.visit('http://localhost:3000/')
+
+        cy.get('input')
+            .eq(0)
+            .type('Clementine')
+            .should('have.value', 'Clementine')
+
+        cy.get('input')
+            .eq(1)
+            .type('02/16')
+            .should('have.value', '02/16')
+        
+        cy.get('input')
+            .eq(2)
+            .type('2:00')
+            .should('have.value', '2:00')
+        
+        cy.get('input')
+            .eq(3)
+            .type(3)
+            .should('have.value', '3')
+    })
+})
