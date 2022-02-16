12291a4bc38360ac05300df61840907117ab033f
Alexis Newby <alexis.newby@gmail.com>
2022-02-16 11:07:47 -0700

    Add Cypress tests for title and form

--- cypress/integration/landing_spec.js
+++ cypress/integration/landing_spec.js
@@ -1,7 +1,46 @@
 describe('Landing Page', () => {
-    it('Should be able to visit the page and render the correct reservation elements', () => {
+    it('Should be able to visit the page and render the correct elements', () => {
         cy.visit('http://localhost:3000/')
+
         cy.get('h1')
             .should('have.text', 'Turing Cafe Reservations')
+
+        cy.get('form')
+            .should('exist')
+
+        cy.get('input')
+            .eq(0)
+            .invoke('attr', 'placeholder')
+            .should('contain', 'Name')
+        
+        cy.get('input')
+            .eq(1)
+            .invoke('attr', 'placeholder')
+            .should('contain', 'Date (mm/dd)')
+       
+        cy.get('input')
+            .eq(2)
+            .invoke('attr', 'placeholder')
+            .should('contain', 'Time')
+
+        cy.get('input')
+            .eq(3)
+            .invoke('attr', 'placeholder')
+            .should('contain', 'Number of guests')
+
+        cy.get('button')
+            .eq(0)
+            .should('contain', 'Make Reservation')
+
+        cy.get('.resy-container')
+            .should('exist')
+
+        cy.get('.reservation-card')
+            .should('have.length', 9)
+
+        cy.get('.reservation-card')
+            .first()
+            
     })
+
 })
